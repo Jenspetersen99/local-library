cf69fa1fa52346e6e67b2bb917f4e274a436c559
Jenspetersen99 <70944848+Jenspetersen99@users.noreply.github.com>
2023-03-13 13:33:18 +0100

    pik

--- express-simplewebsite/routes/index.js
+++ express-simplewebsite/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gruppe 4 er nice' });
+  res.render('index', { title: 'gruppe 4 er gave' });
 });
 
 module.exports = router;
