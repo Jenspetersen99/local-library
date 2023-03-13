09d8935d2e4c886acb7e4fcbc447f988433f1383
Jenspetersen99 <70944848+Jenspetersen99@users.noreply.github.com>
2023-03-13 13:37:42 +0100

    yo

--- express-simplewebsite/routes/index.js
+++ express-simplewebsite/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'gruppe 4 er gave' });
+  res.render('index', { title: 'whigga' });
 });
 
 module.exports = router;
