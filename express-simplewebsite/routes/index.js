5328417c20871e8e4154b6dd807376aaee7c830d
Jenspetersen99 <70944848+Jenspetersen99@users.noreply.github.com>
2023-03-13 13:10:34 +0100

    Update index.js

--- express-simplewebsite/routes/index.js
+++ express-simplewebsite/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'gruppe 4 er nice' });
 });
 
 module.exports = router;
