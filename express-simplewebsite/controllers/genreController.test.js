189bff8c4dc90d7bc089198cc360cbbeab246d61
Jenspetersen99 <70944848+Jenspetersen99@users.noreply.github.com>
2023-04-25 18:12:56 +0200

    nye nice jest test

--- express-simplewebsite/controllers/genreController.test.js
+++ express-simplewebsite/controllers/genreController.test.js
@@ -1,3 +1,16 @@
-test("genre_create_post is a function", ()=>{
-    expect(1).toBe(1)
+const genre_controller = require("../controllers/genreController");
+test("genre_create_post is an object", ()=> {
+    expect(typeof genre_controller.genre_create_post).toBe('object')
+})
+test("genre_create_post cotains a sanitize function", ()=>{
+    const func = genre_controller.genre_create_post[0]
+    expect(typeof func).toBe('function')
+})
+test("genre_create_post contains a function", ()=> {
+    const func = genre_controller.genre_create_post[1]
+    expect(typeof func).toBe('function')
+})
+
+test("genre_delete_post is a function", ()=>{
+    expect(typeof genre_controller.genre_delete_post).toBe('function')
 })
